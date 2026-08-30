54c347d1e0b56a0ad0969e1d87746a46a1f498a8
bernardomfb1 <bernardo150899@icloud.com>
2026-08-30 12:01:00 -0300

    Add files via upload

---
+++ ficha-estudante/src/App.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+
+        String nome = "Bernardo";
+        int idade = 19;
+        String curso = "Engenharia de Software";
+        int periodo = 1;
+        boolean matriculado = true;
+    
+        System.out.println("===== FICHA DO ALUNO =====");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Curso: " + curso);
+        System.out.println("Período: " + periodo);
+        System.out.println("Matriculado: " + matriculado);
+        System.out.println("==========================");
+    }
+}
