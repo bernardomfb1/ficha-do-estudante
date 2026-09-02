343a118132813995b4303ef2565877aafb9086cc
bernardomfb1 <bernardo150899@icloud.com>
2026-09-01 23:54:22 -0300

    Add files via upload

---
+++ media-notas/src/App.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+
+        double nota1 = 7;
+        double nota2 = 8;
+        double nota3 = 9;
+        double media;
+
+        media = nota1 + nota2 + nota3 / 3;
+
+        System.out.println("===== MÉDIA DAS NOTAS =====");
+        System.out.println("Média: " + media);
+        System.out.println("============================");
+    }
+}
